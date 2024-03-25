fc8169ac28bcf70b7e53afd1c255dc40a31600ad
Dmitriy <dikorpshy@mail.ru>
2024-03-25 18:51:44 +0300

    try ex

--- src/pd/prepData.py
+++ src/pd/prepData.py
@@ -11,14 +11,25 @@ class PrepData():
 
     def __init__(self):
         self.__defaultPipline = PrepData.createDefaultPipline()
+        self.status: bool
 
 
     def createDefaultPipline(self):
-        simple_inputer = KNNImputer(n_neighbors = 2)
-        std_scaler = StandardScaler()
-        pipe_num = Pipeline([('imputer', simple_inputer), ('scaler', std_scaler)])
 
-        return pipe_num
+        status_log = ["Create pipline successfull", "Create pipline error"]
+
+        try:
+            simple_inputer = KNNImputer(n_neighbors = 2)
+            std_scaler = StandardScaler()
+            pipe_num = Pipeline([('imputer', simple_inputer), ('scaler', std_scaler)])
+
+            print(status_log[0])
+            return pipe_num
+        
+        except:
+            print(status_log[1])
+            return 0
+
 
 
     @property
@@ -32,22 +43,28 @@ class PrepData():
                         outFilesList: list,
                         pipline: Pipeline = defaultPipline):
         
-        status = {  "Access"    :   "Access", 
-                    "Warning"   :   "Warning",
-                    "Error"     :   "Error"      }
+        status_log = ["Preprocess data finished successfull", "Preprocess data finished error"]
 
-        for url in range(len(inpFilesList)):
-            dataFrame = pd.read_csv(url)
-            newDataFrame = pipline.fit_transform(dataFrame)
-            newDataFrame = pd.DataFrame(newDataFrame, columns=pipline['scaler'].get_feature_names_out(dataFrame.columns))
-            newDataFrame.to_pickle(outFilesList[url])
-        
-        return status["Access"]
+        try:
+            for url in range(len(inpFilesList)):
+                dataFrame = pd.read_csv(url)
+                newDataFrame = pipline.fit_transform(dataFrame)
+                newDataFrame = pd.DataFrame(newDataFrame, columns=pipline['scaler'].get_feature_names_out(dataFrame.columns))
+                newDataFrame.to_pickle(outFilesList[url])
+            
+            print(status_log[0])
+            self.status = True
+            return self.status
+        except:
+            print(status_log[1])
+            self.status = False
+            return self.status
+
     
 
     def is_pass(dataFrame: pd.DataFrame):
         res: bool
-
+        
         
 
         return res
